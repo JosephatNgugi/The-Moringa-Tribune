6ec9af1775e080b2322b9163f6535964d2a47e21
JosephatNgugi <josephat.muriu@student.moringaschool.com>
2022-05-23 23:16:45 +0300

    add-dynamic-url

--- news/views.py
+++ news/views.py
@@ -1,6 +1,6 @@
 import datetime as dt
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse,Http404
 
 # Create your views here.
 def welcome(request):
@@ -29,3 +29,22 @@ def convert_dates(dates):
     # Returning the actual day of the week
     day = days[day_number]
     return day
+
+def past_days_news(request, past_date):
+    try:
+        # Converts data from the string Url
+        date = dt.datetime.strptime(past_date, '%Y-%m-%d').date()
+    
+    except ValueError:
+        # Raise a 404 error when ValueError is thrown
+        raise Http404()
+    
+    day = convert_dates(date)
+    html = f'''
+            <html>
+            <body>
+            <h1>News for {day} {date.day}-{date.month}-{date.year}</h1>
+            </body>
+            </html>
+            '''
+    return HttpResponse(html)
